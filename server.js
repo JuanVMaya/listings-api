c745acd9e3140b635e854a6d11b58bc9102f08ec
theonlyouan <juanjovanegas21@gmail.com>
2022-08-26 15:52:58 -0400

    Create endpoint to retrieve all listings

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-const port=8080;
+const fs = require("fs");
+const port = 8080;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Reusable function to read data from data files
+const readData = (path) => {
+  const dataFile = fs.readFileSync(path);
+  const parsedData = JSON.parse(dataFile);
+  return parsedData;
+};
+
 app.get("/listings", (req, res) => {
-  res.status(200).send("Hello from GET");
+  const listingsData = readData("./data/listings.json");
+  res.status(200).json(listingsData);
 });
 
 app.listen(port, () => {
